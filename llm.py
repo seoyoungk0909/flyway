292dc82259fb9848d78e828f245cbc684283e72c
seoyoungk0909 <skimbj@connect.ust.hk>
2024-07-15 22:11:41 +0800

    load chromadb if directory already exists

--- llm.py
+++ llm.py
@@ -5,12 +5,12 @@ from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunct
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.embeddings import Embeddings
+
 from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.document_loaders import UnstructuredMarkdownLoader
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
-
 from langchain_groq import ChatGroq
 
 import joblib
@@ -18,13 +18,20 @@ import os
 import nest_asyncio  # noqa: E402
 import nltk
 
+# Hide API Key
+from dotenv import load_dotenv
+
+load_dotenv()
+groq_api_key = os.getenv("GROQ_API_KEY")
+llamaparse_api_key = os.getenv("LLAMAPARSE_API_KEY")
+
+# Hide warning message
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 nltk.download("averaged_perceptron_tagger")
 nest_asyncio.apply()
 
-groq_api_key = "groq_api_key"
-llamaparse_api_key = "llamaparse_api_key"
 
-
+# Convert the parsed data into a format that can be used by the vectorstore
 class ChromaEmbeddingsAdapter(Embeddings):
     def __init__(self, ef: EmbeddingFunction):
         self.ef = ef
@@ -46,8 +53,11 @@ class LangChainEmbeddingAdapter(EmbeddingFunction[Documents]):
         return self.ef.embed_documents(input)
 
 
-# Helper function to load or parse the data
 def load_or_parse_data():
+    """
+    This function loads the parsed data from a file if it exists, otherwise it performs the parsing step and stores the result in a file.
+    It utilises llamaparse to parse data from a pdf file.
+    """
     data_file = "data/parsed_data.pkl"
 
     if os.path.exists(data_file):
@@ -98,63 +108,78 @@ def load_or_parse_data():
     return parsed_data
 
 
-# Helper function to load chunks into vectorstore
-def create_vector_database():
+def initialise_llm():
     """
-    Creates a vector database using document loaders and embeddings.
+    Initialise llm by loading or creating a vector database and feeding that to llm model as a retriever.
 
-    This function loads urls,
+    For vector database, it loads urls,
     splits the loaded documents into chunks, transforms them into embeddings using OllamaEmbeddings,
-    and finally persists the embeddings into a Chroma vector database.
+    and persists the embeddings into a Chroma vector database.
 
+    For llm model, it uses ChatGroq with a custom prompt template and a retrieval question answering chain.
     """
-    # Call the function to either load or parse the data
-    llama_parse_documents = load_or_parse_data()
-    # print(llama_parse_documents[0].text[:100])
 
-    with open("data/output.md", "a") as f:  # Open the file in append mode ('a')
-        for doc in llama_parse_documents:
-            f.write(doc.text + "\n")
-
-    markdown_path = "data/output.md"
-    loader = UnstructuredMarkdownLoader(markdown_path)
-
-    documents = loader.load()
-    # Split loaded documents into chunks
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=100)
-    docs = text_splitter.split_documents(documents)
-
-    print(f"length of documents loaded: {len(documents)}")
-    print(f"total number of document chunks generated :{len(docs)}")
+    persist_directory = "chroma_db_llamaparse"
+    collection_name = "rag"
 
     # Initialize Embeddings
-    # embed_model = FastEmbedEmbedding(model_name="BAAI/bge-base-en-v1.5")
     embed_model = ChromaEmbeddingsAdapter(
         SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
     )
 
-    # Create and persist a Chroma vector database from the chunked documents
-    vs = Chroma.from_documents(
-        documents=docs,
-        embedding=embed_model,
-        persist_directory="chroma_db_llamaparse",  # Local mode with in-memory storage only
-        collection_name="rag",
-    )
+    # Load a chroma vector db if it already exists
+    if os.path.exists(persist_directory):
+        print("Loading existing vector database.")
+        vs = Chroma(
+            persist_directory=persist_directory,
+            embedding_function=embed_model,
+            collection_name=collection_name,
+        )
 
-    print("Vector DB created successfully !")
-    # return vs, embed_model
-    return vs
+    # Create a new vector database if it doesn't exist
+    else:
+        print("Creating new vector database.")
+        # Call the function to either load or parse the data
+        llama_parse_documents = load_or_parse_data()
+        # print(llama_parse_documents[0].text[:100])
 
+        with open("data/output.md", "a") as f:  # Open the file in append mode ('a')
+            for doc in llama_parse_documents:
+                f.write(doc.text + "\n")
 
-def get_ai_response(user_input, vs):
-    # Instantiate llm
+        markdown_path = "data/output.md"
+        loader = UnstructuredMarkdownLoader(markdown_path)
+
+        documents = loader.load()
+        # Split loaded documents into chunks
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=2000, chunk_overlap=100
+        )
+        docs = text_splitter.split_documents(documents)
+
+        print(f"length of documents loaded: {len(documents)}")
+        print(f"total number of document chunks generated :{len(docs)}")
+
+        # Create and persist a Chroma vector database from the chunked documents
+        vs = Chroma.from_documents(
+            documents=docs,
+            embedding=embed_model,
+            persist_directory=persist_directory,  # Local mode with in-memory storage only
+            collection_name=collection_name,
+        )
+
+        print("Vector DB created successfully !")
+
+    # Initialize llm model
     chat_model = ChatGroq(
         temperature=0,
         model_name="mixtral-8x7b-32768",
         api_key=groq_api_key,
     )
 
-    retriever = vs.as_retriever(search_kwargs={"k": 3})
+    # Create a retriever from the vector database
+    retriever = vs.as_retriever()
+
     # Create custom prompt template
     custom_prompt_template = """Use the following pieces of information to answer the question.
     If you don't know the answer, just say that you don't know, don't try to make up an answer.
@@ -170,21 +195,6 @@ def get_ai_response(user_input, vs):
         template=custom_prompt_template, input_variables=["context", "question"]
     )
 
-    ########################### RESPONSE ###########################
-    PromptTemplate(
-        input_variables=["context", "question"],
-        template=(
-            """Use the following pieces of information to answer the question.
-    If you don't know the answer, just say that you don't know, don't try to make up an answer.
-
-    Context: {context}
-    Question: {question}
-
-    Only return the helpful answer below and nothing else.
-    Helpful answer:"""
-        ),
-    )
-
     # Instantiate retrieval question answering chain
     qa = RetrievalQA.from_chain_type(
         llm=chat_model,
@@ -194,10 +204,13 @@ def get_ai_response(user_input, vs):
         chain_type_kwargs={"prompt": prompt},
     )
 
+    return qa
+
+
+# Retrieve response by invoking the QA Chain
+def get_ai_response(user_input):
+    qa = initialise_llm()
+
     # Invoke the QA Chain
-    query = user_input
-    response = qa.invoke({"query": query})
+    response = qa.invoke({"query": user_input})
     return response["result"]
-
-
-# print(response)
